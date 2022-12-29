24d38ef55e10ebca9ea36a3d1a2bf9f4f8b793f5
duycc <gz_duyong@163.com>
2022-12-29 15:57:33 +0800

    strtoslice

--- convert.go
+++ convert.go
@@ -3,6 +3,7 @@ package util
 import (
 	"encoding/json"
 	"strconv"
+	"strings"
 )
 
 func StrToInt(str string) int {
@@ -13,6 +14,16 @@ func StrToInt(str string) int {
 func StrToInt64(str string) int64 {
 	num, _ := strconv.ParseInt(str, 10, 64)
 	return num
+}
+
+func StrToInt64Slice(str, split string) []int64 {
+	strs := strings.Split(str, split)
+	res := []int64{}
+	for _, v := range strs {
+		num, _ := strconv.ParseInt(v, 10, 64)
+		res = append(res, num)
+	}
+	return res
 }
 
 func StructToMap(object interface{}) (map[string]interface{}, error) {
